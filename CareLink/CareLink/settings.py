c7bab858a04a715a05e9a07da0117ded34008e7a
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-05-25 19:51:57 +0200

    New Setting (Local Init) + Set Up Database

--- CareLink/CareLink/settings.py
+++ CareLink/CareLink/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'CareLink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carelink',  # Replace with your database name
+        'USER': 'root',  # Replace with your MySQL username
+        'PASSWORD': '',  # Replace with your MySQL password
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
