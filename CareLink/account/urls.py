3d00708d21aca6b15d24a55940ce53ed3f5294e5
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-05-26 13:07:46 +0200

    Security renforcement - Implement scripts Fixtures - Add URL/View (Login , LogOut, Register)

---
+++ CareLink/account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views.register import RegisterAPIView
+from .views.login import LoginAPIView 
+from .views.logout import LogoutAPIView
+
+
+
+urlpatterns = [
+    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+]
