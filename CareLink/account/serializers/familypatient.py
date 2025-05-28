443caeab530518117e1f4875988278f331e6a7a5
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-05-28 19:03:12 +0200

    Implement relationship family [API]

--- CareLink/account/serializers/familypatient.py
+++ CareLink/account/serializers/familypatient.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from CareLink.models import FamilyPatient
+from account.serializers.user import UserSerializer
+from account.serializers.patient import PatientSerializer
 
 class FamilyPatientSerializer(serializers.ModelSerializer):
+    patient = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = FamilyPatient
-        fields = ['id', 'link', 'patient_id', 'user_id']
+        fields = ['id', 'link', 'patient', 'user']
+
+    def get_patient(self, obj):
+        if obj.patient:
+            return PatientSerializer(obj.patient).data
+        return None
+
+    def get_user(self, obj):
+        if obj.user:
+            user_data = UserSerializer(obj.user).data
+            user_data['full_name'] = f"{obj.user.firstname} {obj.user.lastname}"  # Add full name
+            return user_data
+        return None
