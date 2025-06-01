7d3adb2d2cfe72e2777990912c5eaa1896dc1d3e
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-06-01 15:29:52 +0200

    Coordinator - Patients page Done

--- CareLink/account/views/check/check_role.py
+++ CareLink/account/views/check/check_role.py
@@ -1,13 +1,6 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from CareLink.models import User
 
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-@require_http_methods(["GET"])
 def get_user_role(request, user_id):
     try:
         user = User.objects.get(id=user_id)
