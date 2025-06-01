2979d64e422d00ad2810bb66532cc7aaa3a5a777
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-06-01 15:30:33 +0200

    Create Patient view + Medical folder + Add Entry

---
+++ carelink-front/src/pages/CoordinatorToolbar.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './CoordinatorToolbar.css';
+
+const CoordinatorToolbar = () => {
+  const [toolbarData, setToolbarData] = useState(null);
+
+  useEffect(() => {
+    fetch('/coordinator/toolbar')
+      .then((response) => response.json())
+      .then((data) => setToolbarData(data))
+      .catch((error) => console.error('Error fetching toolbar data:', error));
+  }, []);
+
+  if (!toolbarData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="coordinator-toolbar">
+      <nav className="menu">
+        {toolbarData.menu.map((item, index) => (
+          <a key={index} href={item.url} className="menu-item">
+            {item.name}
+          </a>
+        ))}
+      </nav>
+      <div className="actions">
+        {toolbarData.actions.map((action, index) => (
+          <button key={index} onClick={() => window.location.href = action.url} className="action-button">
+            {action.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CoordinatorToolbar;
